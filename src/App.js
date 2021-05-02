b69d4e1ad8312dc164397c20777eaa79f5b215b2
nick <zhangsnake28@gmail.com>
2021-05-02 12:39:49 -0700

    Start with orange theme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {Button,message} from 'antd'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.less'
+
+export default class App extends Component {
+  clickMessage = () => {
+    message.success('test message')
+  }
+
+  render() {
+    return (
+      <div>
+        <Button type="primary" onClick = {this.clickMessage}>Button</Button>
+      </div>
+    )
+  }
 }
 
-export default App;
